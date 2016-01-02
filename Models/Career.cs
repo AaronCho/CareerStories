c85934d4aa4ae0c998543bb822f92d3b8e90a827
AaronCho <aaron-cho@hotmail.com>
2016-01-01 23:21:46 -0800

    added Story and Career models

---
+++ Models/Career.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareerStories.Models
+{
+    public class Career
+    {
+        public long Id { get; set; }
+
+        public string CareerName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Models/Story.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareerStories.Models
+{
+    public class Story
+    {
+        public long Id { get; set; }
+        public long CommentCount { get; set; }
+
+        public string Story { get; set; }
+
+        public DateTime PostDate { get; set; }     
+    }
+}
