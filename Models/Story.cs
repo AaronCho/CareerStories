c446d9397be65cab75c1f70493bbd7cc36fa5b78
AaronCho <aaron-cho@hotmail.com>
2016-01-04 20:53:43 -0800

    added Story and Career models

--- Models/Story.cs
+++ Models/Story.cs
@@ -10,7 +10,7 @@ namespace CareerStories.Models
         public long Id { get; set; }
         public long CommentCount { get; set; }
 
-        public string Story { get; set; }
+        public string CareerStory { get; set; }
 
         public DateTime PostDate { get; set; }     
     }
