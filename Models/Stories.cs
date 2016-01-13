e528e316e39b3e7ea2f994ececd91c27e23cd185
AaronCho <aaron-cho@hotmail.com>
2016-01-12 16:29:44 -0800

    added Story view, finished Create action redirects

--- Models/Stories.cs
+++ Models/Stories.cs
@@ -33,10 +33,16 @@ namespace CareerStories.Models
         [StringLength(maximumLength: 20000, MinimumLength = 200)]
         public string Story { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 200, MinimumLength = 0)]
         public string Title { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 0)]
         public string Education { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 0)]
         public string Company { get; set; }
 
         [Required]
--- Models/StoriesViewModel.cs
+++ Models/StoriesViewModel.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace CareerStories.Models
 {
     public class StoriesViewModel
-    {      
+    {
+        [Required]
+        [StringLength(maximumLength: 20000, MinimumLength = 200)]
         public string Story { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 200, MinimumLength = 0)]
         public string Title { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 0)]
         public string Education { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 0)]
         public string Company { get; set; }
-       
-        public DateTime PostDate { get; set; }
     }
 }
